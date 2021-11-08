9dcba30bdacdacfc888c61a425bd8e93e5c30c31
Yangyu Chen <cyy@cyyself.name>
2021-11-08 16:02:25 +0800

    ops_multiplier in yaffs_fileem2k should be extern

--- direct/test-framework/yaffs_fileem2k.c
+++ direct/test-framework/yaffs_fileem2k.c
@@ -90,7 +90,7 @@ extern int simulate_power_failure;
 static int remaining_ops;
 static int nops_so_far;
 
-int ops_multiplier;
+extern int ops_multiplier;
 
 static void yflash2_MaybePowerFail(unsigned int nand_chunk, int failPoint)
 {
